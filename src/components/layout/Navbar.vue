a92f4e48c6fdff494e1930a4efc66fff4598799e
milktegi <milkteegi@gmail.com>
2018-11-25 13:21:22 +0900

    Added Google map

---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+	
+<div class="navbar">
+<nav class="deep-purple darken-1">
+	<div class="container">
+		<a href="" class="brand-logo left">geo-marios</a>
+		<ul class="right">
+			<li><a href="">로그인</a></li>
+			<li><a href="">회원가입</a></li>
+		</ul>
+	</div>
+</nav>
+</div>
+
+</template>
+
+<script>
+export default {
+	name: 'Navbar',
+	data() {
+		return {
+
+		}
+	}
+
+}
+</script>
+
+<style>
+.navbar nav {
+    padding: 0 30px
+}
+
+</style>
