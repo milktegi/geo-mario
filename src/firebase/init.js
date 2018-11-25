4992f9bc0102bae39454120f6f0baeaf22b36080
milktegi <milkteegi@gmail.com>
2018-11-25 10:44:59 +0900

    set up init files(firebase)

---
+++ src/firebase/init.js
@@ -0,0 +1,18 @@
+ import firebase from 'firebase'
+ import firestore from 'firebase/firestore'
+ 
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBfWJ739R_iQ4VXhY_gi1hmB4_CFz_KB9o",
+    authDomain: "geo-mario.firebaseapp.com",
+    databaseURL: "https://geo-mario.firebaseio.com",
+    projectId: "geo-mario",
+    storageBucket: "geo-mario.appspot.com",
+    messagingSenderId: "135621376194"
+  };
+
+
+const firebaseApp =  firebase.initializeApp(config);
+
+export default firebaseApp.firestore()
