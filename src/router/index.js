5035de4026d0dbdb89d7326af04d4aa76b82e705
milktegi <milkteegi@gmail.com>
2018-11-26 15:13:50 +0900

    created user profile, router_1

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import GMap from '@/components/home/GMap'
+import ViewProfile from '@/profile/ViewProfile'
 import Signup from '@/components/auth/Signup'
 import Login from '@/components/auth/Login'
 import firebase from 'firebase'
@@ -26,6 +27,14 @@ const router = new Router({
       path: '/login',
       name: 'Login',
       component: Login
+    },
+     {
+      path: '/profile/:id',
+      name: 'ViewProfile',
+      component: ViewProfile,
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
 })
